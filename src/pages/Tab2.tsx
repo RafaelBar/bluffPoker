5d5f0f8c937c0989d7f57563e95517e80328f907
Rafael Bar <rafaelbar37@gmail.com>
2022-03-04 23:54:29 +0200

    sass added to project

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,24 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './Tab2.scss';
 
 const Tab2: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
+    <div className='tab2'>tab2</div>
   );
 };
 
