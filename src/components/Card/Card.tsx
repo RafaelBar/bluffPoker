d1ca2ea9fd24aa31c0c386e7471e0e1a9a4bfcc9
Rafael Bar <rafaelbar37@gmail.com>
2022-03-05 20:39:51 +0200

    card component added

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,47 @@
+import './Card.scss';
+import classnames from 'classnames';
+
+interface cardProps {
+    id: string,
+    group: 'hearts' | 'clubs' | 'diams' | 'spades',
+    label: string
+}
+
+const Card: React.FC<cardProps> = ({
+    id,
+    group,
+    label
+}) => {
+  return (
+    <div className='card'>
+
+        <label className={classnames(
+          'small float',
+          group
+        )}>{label}
+          {group === 'hearts' && <span>&hearts;</span>}
+          {group === 'diams' && <span>&diams;</span>}
+          {group === 'clubs' && <span>&clubs;</span>}
+          {group === 'spades' && <span>&spades;</span>}
+        </label>
+
+        <label className={classnames(
+          'large main',
+          group
+        )}>{label}</label>
+
+        <label className={classnames(
+          'small float mirror',
+          group
+        )}>{label}
+            {group === 'hearts' && <span>&hearts;</span>}
+            {group === 'diams' && <span>&diams;</span>}
+            {group === 'clubs' && <span>&clubs;</span>}
+            {group === 'spades' && <span>&spades;</span>}
+        </label>
+
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Tab1.tsx
+++
@@ -1,9 +0,0 @@
-import './Tab1.scss';
-
-const Tab1: React.FC = () => {
-  return (
-    <div className='tab1'>tab1</div>
-  );
-};
-
-export default Tab1;
